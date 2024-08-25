b507ddb5b402603657bd3d09a6b7af7b2258ab9e
janehutorova <jeniavizir@gmail.com>
2024-08-25 20:40:26 +0100

    Ceasar cipher completed. I shortened a code for readability and added some more logic to ask if user wants to continue or no

--- day_6/caesar_cipher.py
+++ day_6/caesar_cipher.py
@@ -1,36 +1,34 @@
 alphabet = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ]
-direction = input("Type 'encode' to encript and 'decode' to decrypt\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-def encrypt(text,shift):
-    encrypted_text = ""
-    for letter in text:
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *=-1
+            
+    for letter in original_text:
         if letter in alphabet:
-            original_index = alphabet.index(letter)
-            new_index = (original_index + shift) % len(alphabet)
-            encrypted_text+= alphabet[new_index]
-        else:
-            encrypted_text_text += letter
-    return encrypted_text    
-
-if direction == "encode":
-        encripted_message = encrypt(text, shift)
-        print(f"Encoded message is:\n{encripted_message}")
-
-def decrypt(text, shift):
-    decrypted_text = ""
-    for letter in text:
-         if letter in alphabet:
             index = alphabet.index(letter)
-            original_index = (index - shift) % len(alphabet)
-            decrypted_text+= alphabet[original_index]
-    return decrypted_text
-if direction == "decode":
-     decoded_message = decrypt(text, shift)
-     print(f"Decoded message is:\n{decoded_message}")
+            original_index = (index + shift_amount) % len(alphabet)
+            output_text += alphabet[original_index]
+        else:
+            output_text += letter
+    print(f"Here is the {encode_or_decode}d result:\n{output_text}")
 
-def caesar(direction, text, shift):
+game_over = False
+
+while not game_over:
+    direction = input("Type 'encode' to encrypt and 'decode' to decrypt\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    caesar(text, shift, direction)
+    
+    continue_game = input("Would you like to go again? Type 'yes' or 'no'\n").lower()
+    if continue_game == "no":
+        game_over = True
+        print("Game is over!")
+   
+
      

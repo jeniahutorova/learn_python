e4f2bc854e0268f98fde850c62c38a76f5e549ff
Yevheniia Hutorova <jeniavizir@gmail.com>
2024-09-10 14:56:05 +0100

    Created a quiz game using OOP principles

---
+++ day_14/data.py
@@ -0,0 +1,42 @@
+question_data = [
+    {
+        "text": "The word \"Inception\" came from the 2010 blockbuster hit \"Inception\".",
+        "answer": "False"
+    },
+    {
+        "text": "Gumbo is a stew that originated in Louisiana.",
+        "answer": "True"
+    },
+    {
+        "text": "Linus Torvalds created Linux and Git.",
+        "answer": "True"
+    },
+    {
+        "text": "\"Elementary, my dear Watson\" is a phrase that is never truly said within the Conan Doyle books of Sherlock Holmes.",
+        "answer": "True"
+    },
+    {
+        "text": "The Angry Video Game Nerd's alter ego is \"Board James\".",
+        "answer": "True"
+    },
+    {
+        "text": "Ana was added as a new hero for the game Overwatch on July 19th, 2016.",
+        "answer": "True"
+    },
+    {
+        "text": "The United States was a member of the League of Nations.",
+        "answer": "False"
+    },
+    {
+        "text": "In RuneScape, one must complete the \"Dragon Slayer\" quest before equipping Rune Platelegs.",
+        "answer": "False"
+    },
+    {
+        "text": "Solid Snake is actually a clone from the DNA of Big Boss in the Metal Gear Solid series' history.",
+        "answer": "True"
+    },
+    {
+        "text": "The Sun rises from the North.",
+        "answer": "False"
+    }
+]

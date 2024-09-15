c5afeb6df9de9d27b6858b627ea83087bed67636
Yevheniia Hutorova <jeniavizir@gmail.com>
2024-09-15 10:57:34 +0100

    Made changes to the logic to have opportunity to draw 10 circles in a row and then come back to the next one

--- myenv/hirst_painting.py
+++ myenv/hirst_painting.py
@@ -21,13 +21,14 @@ cursor.speed(0)
 screen = t.Screen()
 color_list = [(207, 159, 82), (54, 89, 130), (146, 91, 39), (140, 26, 48), (222, 207, 104), (132, 177, 203), (158, 46, 83), (45, 55, 104), (170, 160, 39), (129, 189, 143), (83, 20, 44), (36, 43, 69), (186, 94, 106), (186, 140, 172), (84, 120, 180), (60, 39, 31), (88, 157, 92), (78, 153, 164), (195, 78, 72), (45, 74, 78), (80, 74, 44), (162, 201, 218), (58, 126, 122), (218, 176, 187), (169, 207, 170), (220, 181, 168)]
 
+cursor.penup()
 cursor.setheading(225)
 cursor.forward(300)
 cursor.setheading(0)
 
 circles_per_row = 10 
 
-for _ in range(20):
+for _ in range(10):
     for circle in range(circles_per_row):
         color = random.choice(color_list)
         cursor.pencolor(color)
@@ -40,12 +41,14 @@ for _ in range(20):
         cursor.penup()
         cursor.forward(50)
         cursor.pendown()
-screen.exitonclick()
 
-cursor.penup()
-cursor.setheading(270)  # Point down
-cursor.forward(50)  # Move down to the next row
-cursor.setheading(180)  # Point left
-cursor.forward(500)  # Move to the start of the row
-cursor.setheading(0)  # Reset direction to right
-cursor.pendown()
+
+    cursor.penup()
+    cursor.setheading(90)  # Point up
+    cursor.forward(50)  # Move up to the next row
+    cursor.setheading(180)  # Point left
+    cursor.forward(500)  # Move to the start of the row
+    cursor.setheading(0)  # Reset direction to right
+    cursor.pendown()
+
+screen.exitonclick()

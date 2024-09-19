61acc535a8c8a37103ffae51546529661a74139b
janehutorova <jeniavizir@gmail.com>
2024-09-19 18:56:10 +0100

    Updated snake game with new class to make the code more readable

--- day_17/main.py
+++ day_17/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 pen = Turtle()
 screen = Screen()
@@ -8,21 +9,20 @@ screen.bgcolor("black")
 screen.title("My snake game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_is_on = True
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
 while game_is_on:
     screen.update() # Updates screen after loop is finished
     time.sleep(0.1)
-    for segment in segments:
-        segment.forward(20)
-        
+    
+    snake.move()
+
 screen.exitonclick()

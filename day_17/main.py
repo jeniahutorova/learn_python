21e5a40a2f44c3b55b5f41f7ca1dafdeb40d8f75
Yevheniia Hutorova <jeniavizir@gmail.com>
2024-09-20 12:10:02 +0100

    Finished creating a snake game, added more classes and modified some logic for better readability

--- day_17/main.py
+++ day_17/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 pen = Turtle()
 screen = Screen()
@@ -10,7 +12,8 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+scoreboard = Scoreboard()
 game_is_on = True
 
 screen.listen()
@@ -22,7 +25,20 @@ screen.onkey(snake.left, "Left")
 while game_is_on:
     screen.update() # Updates screen after loop is finished
     time.sleep(0.1)
-    
     snake.move()
 
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        snake.extend()
+        food.refresh()
+        scoreboard.score_up()
+    # Detect collision with walls.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+    # Detect collision with tail.
+    for segment in snake.segments[1:]:#  Slice everything from 1 to the end
+         if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 screen.exitonclick()
---
+++ day_17/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 20, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle() # We don't want to see a turtle
+        self.goto(0, 260)
+        self.update_score()
+
+    def update_score(self):
+        self.clear() # Clear previous score before updating
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # We're calling a score_up function only when the player earns points
+    def score_up(self):
+        self.score += 1
+        self.update_score() 

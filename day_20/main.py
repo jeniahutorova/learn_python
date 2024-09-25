d5a77ec0f20130c5396edf49a11cfc579b32fd32
Yevheniia Hutorova <jeniavizir@gmail.com>
2024-09-25 10:15:32 +0100

    Started learning how to open and read or open and write the file

---
+++ day_20/main.py
@@ -0,0 +1,6 @@
+# with open(r"C:\Users\jenia\Desktop\learn_python\day_20\my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("new_file.txt", mode="w") as file:
+    file.write("New text.")

57c5919886612271420379fd97ea7f8caad6182a
Yevheniia Hutorova <jeniavizir@gmail.com>
2024-10-03 11:11:00 +0100

    Started creating bits for upcoming project

--- day_23/main.py
+++ day_23/main.py
@@ -1,12 +1,22 @@
-import tkinter
+from tkinter import *
 
+def button_clicked():
+    data = input.get()
+    my_label.config(text=data)
 
-window = tkinter.Tk()
+# Creating the main window
+window = Tk()
 window.title("My first GUI program")
 window.minsize(500, 300)
 
-
-
-my_label = tkinter.Label(window, text ="I\'m a label", font =("Arial", 24, "bold"))
+# Create and pack lable
+my_label = Label(window, text ="I\'m a label", font =("Arial", 24, "bold"))
 my_label.pack()
+
+button = Button(window, text = "Click me", command=button_clicked)
+button.pack()
+
+input = Entry()
+input.pack()
+# Start the GUI event loop
 window.mainloop()
